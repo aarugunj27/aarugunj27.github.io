db11b722bb902d306b76cdb5ae7f31a295fecb99
Aarush Gunjal <aarugunj27@ncs.charter.k12.de.us>
2023-03-27 16:36:06 -0400

    new changes

---
+++ index.js
@@ -0,0 +1,7 @@
+const html = document.documentElement
+const canvas = document.querySelector('.visualization-scrolling');
+const context = canvas.getContext('2d');
+
+const currentFrame = index => (
+    ''
+)
